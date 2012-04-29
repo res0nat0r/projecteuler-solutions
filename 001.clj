68ef828119073991598c9e6152888ad454a7dc2f
Stefhen Hovland <stefhen.hovland+github@gmail.com>
2012-04-28 22:13:44 -0700

    added 002.clj

--- 001.clj
+++ 001.clj
@@ -1,1 +1,5 @@
-(println (reduce + (filter #(or (= (mod % 3) 0) (= (mod % 5) 0))(range 1000))))
+(println (reduce +
+  (filter
+    #(or
+      (= (mod % 3) 0)
+      (= (mod % 5) 0))(range 1000))))
