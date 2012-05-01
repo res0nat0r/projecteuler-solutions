febc259dcddef0c1400b05f2b841b1357256682b
Stefhen Hovland <stefhen.hovland+github@gmail.com>
2012-04-30 19:07:16 -0700

    001.clj

--- 001.clj
+++ 001.clj
@@ -1,5 +1,5 @@
-(println (reduce +
-  (filter
-    #(or
-      (= (mod % 3) 0)
-      (= (mod % 5) 0))(range 1000))))
+(defn multiple-of-3-or-5? [n]
+  (or (zero? (rem n 3))
+      (zero? (rem n 5))))
+
+(apply + (filter #(multiple-of-3-or-5? %) (range 1 1000)))
