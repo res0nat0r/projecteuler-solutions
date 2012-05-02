83dc11a82d2e25ba967e2a7aa19145402d15e04c
Stefhen Hovland <stefhen.hovland+github@gmail.com>
2012-05-02 16:22:32 -0700

    added 003.clj

---
+++ 003.clj
@@ -0,0 +1,10 @@
+(defn factor? [n d]
+  (zero? (mod n d)))
+
+(defn factors [n]
+  (filter #(factor? n %) (range 2 n)))
+
+(defn prime? [n]
+  (empty? (factors n)))
+
+(println (apply max (take-while prime? (factors 600851475143))))
