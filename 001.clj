13d78a005ae19e264c63d5a9985adcd328b0df67
Stefhen Hovland <stefhen.hovland+github@gmail.com>
2012-05-02 16:22:24 -0700

    added println

--- 001.clj
+++ 001.clj
@@ -2,4 +2,4 @@
   (or (zero? (rem n 3))
       (zero? (rem n 5))))
 
-(apply + (filter #(multiple-of-3-or-5? %) (range 1 1000)))
+(println (apply + (filter #(multiple-of-3-or-5? %) (range 1 1000))))
