d6a3742e86e46ec300c1d44ffa611c0c454365b7
Stefhen Hovland <stefhen.hovland+github@gmail.com>
2012-05-02 16:22:03 -0700

    added println

--- 002.clj
+++ 002.clj
@@ -3,4 +3,4 @@
   (if (= n 1) 1
   (+ (fib (- n 1)) (fib (- n 2))))))
 
-(apply + (filter even? (take-while #(< % 4000000) (map #(fib %) (range 1 40)))))
+(println (apply + (filter even? (take-while #(< % 4000000) (map #(fib %) (range 1 35))))))
