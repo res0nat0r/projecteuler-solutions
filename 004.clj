d5477c4489142c0f995fe6465cfeafdddd2a00cb
Stefhen Hovland <stefhen.hovland+github@gmail.com>
2012-05-02 20:02:33 -0700

    updated iterator

--- 004.clj
+++ 004.clj
@@ -3,4 +3,4 @@
     (reverse (str n)) 
     (seq (str n))))
 
-(println (apply max (filter palindrome?  (map #(* %1 %2) (range 100 1000) (range 100 1000)))))
+(apply max (filter palindrome? (for [x (range 100 1000) y (range 100 1000)] (* x y))))
