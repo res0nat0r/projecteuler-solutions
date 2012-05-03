e32315af8ccc991c691c4635e9d3d0dc8ffe4837
Stefhen Hovland <stefhen.hovland+github@gmail.com>
2012-05-02 21:23:01 -0700

    added 005.clj

---
+++ 005.clj
@@ -0,0 +1,9 @@
+(defn factor? [n d]
+  (zero? (mod n d)))
+
+(defn factors-upto-20? [n]
+  (= 
+    (count (filter #(factor? n %) (range 1 21))) 
+    20))
+
+(nth (filter #(factors-upto-20? %) (range)) 1)
