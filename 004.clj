0a824e0c6344b65ef1dad189fa55f95a3986845d
Stefhen Hovland <stefhen@rightscale.com>
2015-06-19 13:19:10 -0400

    Add println

--- 004.clj
+++ 004.clj
@@ -5,4 +5,4 @@
     (reverse (str n)) 
     (seq (str n))))
 
-(apply max (filter palindrome? (for [x (range 100 1000) y (range 100 1000)] (* x y))))
+(println (apply max (filter palindrome? (for [x (range 100 1000) y (range 100 1000)] (* x y)))))
