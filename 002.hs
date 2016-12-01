93bd7734cff37e96a7e15f8619d3999e9b5ca590
Stefhen Hovland <stefhen.hovland+github@gmail.com>
2016-11-30 20:08:41 -0500

    A  002.hs

---
+++ 002.hs
@@ -0,0 +1,10 @@
+-- By considering the terms in the Fibonacci sequence whose values
+-- do not exceed four million, find the sum of the even-valued terms.
+
+module Main where
+ 
+fibo :: Integer -> Integer
+fibo n = fst $ foldl (\(a, b) _ -> (b, a + b)) (0, 1) [1 .. n]
+
+main = do
+  putStrLn $ show $ sum $ filter even $ takeWhile (< 4000000) $ map fibo [1..]
