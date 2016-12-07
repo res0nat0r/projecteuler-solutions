108c2746cfda1cb061df88b9dd3841092ae299c5
Stefhen Hovland <stefhen.hovland+github@gmail.com>
2016-12-07 01:26:39 -0500

    use print and added answer

--- 002.hs
+++ 002.hs
@@ -6,5 +6,6 @@ module Main where
 fibo :: Integer -> Integer
 fibo n = fst $ foldl (\(a, b) _ -> (b, a + b)) (0, 1) [1 .. n]
 
-main = putStrLn (show answer)
-answer = sum $ filter even $ takeWhile (< 4000000) $ map fibo [1..]
+main = print $ sum $ filter even $ takeWhile (< 4000000) $ map fibo [1..]
+
+-- 4613732
