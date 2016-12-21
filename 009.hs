b5ac6ad1af9007751fa32f64fd4e9d52944504d6
Stefhen Hovland <stefhen.hovland+github@gmail.com>
2016-12-20 23:11:31 -0500

    Add 009.hs

---
+++ 009.hs
@@ -0,0 +1,22 @@
+{-
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+-}
+
+main = print answer
+
+answer = product $ head $ 
+  [ [a,b,c] |
+    a<-[1..997], 
+    b<-[1..997], 
+    c<-[1..997], 
+    a < b, 
+    b < c, 
+    a^2 + b^2 == c^2, a + b + c == 1000]
+
+-- 31875000
