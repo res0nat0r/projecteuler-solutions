a314e7c01da94be7552151e3b89aba76a1d4924a
Stefhen Hovland <stefhen.hovland@gmail.com>
2020-01-15 23:51:41 -0600

    wip

--- p025/src/p025/core.clj
+++ p025/src/p025/core.clj
@@ -2,11 +2,17 @@
 
 (def fib
   (lazy-cat [0.0 1.0] (map + fib (rest fib))))
+;
+;(println (take 30 fib))
+;
+;(println (map #(format "%.0f" %) (take 30 fib)))
+;
+;
+;
+;(println (map #(count (str (format "%.0f" %))) (take 30 fib)))
 
-(println (take 30 fib))
+(defn -main [& _]
+  ; (println (last (take-while #(<= % 1000) (map #(count (str (format "%.0f" %))) fib))))
 
-(println (map #(format "%.0f" %) (take 30 fib)))
-
-
-
-(println (map #(count (str (format "%.0f" %))) (take 30 fib)))
+  (println (map #(format "%.0f" %) (take 10 fib)))
+)
