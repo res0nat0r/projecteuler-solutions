dba85a1daec4e074b116cf9aba8bb5597b9f2127
Stefhen Hovland <stefhen.hovland@gmail.com>
2020-01-21 23:46:55 -0600

    p031

---
+++ p031/src/p031/core.clj
@@ -0,0 +1,4 @@
+(ns p031.core
+  (:gen-class))
+
+(require 'clojure.core.logic)
---
+++ p031/test/p031/core_test.clj
@@ -0,0 +1,7 @@
+(ns p031.core-test
+  (:require [clojure.test :refer :all]
+            [p031.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
