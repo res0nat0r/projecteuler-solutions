330e1699632300b628c4cd23526678c139200835
Stefhen Hovland <stefhen.hovland@gmail.com>
2020-01-22 00:25:05 -0600

    wip

--- p694/src/p694/core.clj
+++ p694/src/p694/core.clj
@@ -2,13 +2,18 @@
   (:gen-class))
 
 (defn factor? [n d]
-  (zero? (rem n d)))
+  (zero? (mod n d)))
 
-(defn factors [n]
-  (filter #(factor? n %) (range 2 n)))
+(def factors
+  (memoize
+    (fn [n]
+      (filter #(factor? n %) (range 2 (bigint (Math/sqrt (inc n))))))))
 
 (defn prime? [n]
   (empty? (factors n)))
 
 (defn prime-factors [n]
   (filter #(prime? %) (factors n)))
+
+
+
