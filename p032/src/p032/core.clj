b889820a705dbbd7f67557dd2b3c65b9665e08bf
Stefhen Hovland <stefhen.hovland@gmail.com>
2020-01-24 23:46:41 -0600

    wip

--- p032/src/p032/core.clj
+++ p032/src/p032/core.clj
@@ -5,8 +5,25 @@
   (->> n str (map (comp read-string str))))
 
 (defn pandigital? [n]
-  (let [length-n (count (str n))
-        digits-of-n (digits n)
+  (let [length-n      (count (str n))
+        digits-of-n   (digits n)
         digits-1-to-n (set (range 1 (inc length-n)))]
     (every? digits-1-to-n digits-of-n)))
 
+(def multiples
+  (for [a (range 1 1000)
+        b (range 1 1000)]
+    (concat [a] [b] [(* a b)])))
+
+(def multiples*
+  (for [a (range 1 1000)
+        b (range 1 1000)]
+    (clojure.string/join (concat [a] [b] [(* a b)]))))
+
+(def xxx
+  (for [a (range 1 1000)
+        b (range 1 1000)]
+
+    [(concat [a] [b] [(* a b)])
+     (read-string (clojure.string/join (concat [a] [b] [(* a b)])))]
+    ))
