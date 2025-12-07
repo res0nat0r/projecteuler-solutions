9bb924cae78dfeb05cdeaf52d74284f7636d1e48
Stefhen Hovland <stefhen.hovland@gmail.com>
2025-12-07 11:45:36 -0600

    fix

--- 020.hs
+++ 020.hs
@@ -13,6 +13,6 @@ module Main where
 
 factorial n = product [1 .. n]
 
-main =    print $ sum (map (read . (: "")) (show $ factorial 100))
+main = print $ sum (map (read . (: "")) (show $ factorial 100))
 
 -- 648
