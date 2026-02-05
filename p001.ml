01cafa8ed508bb41bc14b2a638236d523a8a9d11
Stefhen Hovland <stefhen.hovland@gmail.com>
2026-02-04 19:06:56 -0600

    init

---
+++ p001.ml
@@ -0,0 +1,12 @@
+(*
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+
+module Main where
+
+main = print $ sum [x | x <- [1 .. 999], mod x 3 == 0 || mod x 5 == 0]
+
+-- 233168
+*)
+
+open! Base
+let range = List.range  1 1000
