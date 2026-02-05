ce26f53985617ed70aba3d50640628933b698f93
Stefhen Hovland <stefhen.hovland@gmail.com>
2026-02-04 23:29:06 -0600

    fmt

--- 018.hs
+++ 018.hs
@@ -4,8 +4,8 @@ the row below, the maximum total from top to bottom is 23 .
 
                         3
                        7 4
-                       2 4 6
-                      8 5 9 3
+                      2 4 6
+                     8 5 9 3
 
 That is, 3 +7 +4 +9 =23.
 
